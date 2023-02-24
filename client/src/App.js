239dfddaa31e537d9fb8714562c41f70f19463a6
Manav Naharwal <manavnaharwalp@gmail.com>
2023-02-24 16:05:34 +0530

    Added today match comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Layout from './components/navigation/Layout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import LeaderBoard from './pages/LeaderBoard';
 import MakePrediction from './pages/MakePrediction';
 import { Route, Link, Routes } from 'react-router-dom'  
-import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
-    <Routes>  
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/leader_board" element={<LeaderBoard/>} />
-        <Route path="/make_prediction" element={<MakePrediction/>} />
-    </Routes> 
+    <Layout>
+      <Routes>  
+          <Route path="/" element={<MakePrediction/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/leaderboard" element={<LeaderBoard/>} />
+      </Routes> 
+    </Layout>
   );
 }
 
---
+++ client/src/pages/MakePrediction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodaysMatch from '../components/prediction/TodaysMatch'
+
+const MakePrediction = () => {
+  return (
+    <div>
+        <TodaysMatch/>
+    </div>
+  )
+}
+
+export default MakePrediction
