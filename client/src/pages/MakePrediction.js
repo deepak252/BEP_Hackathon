9d768ac2449de728b8142454fb81cfea38b7dad4
Manav Naharwal <manavnaharwalp@gmail.com>
2023-02-24 16:48:56 +0530

    Add date picker

--- client/src/pages/MakePrediction.js
+++ client/src/pages/MakePrediction.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import TodaysMatch from '../components/prediction/TodaysMatch'
+import DatePicker from "react-datepicker";
+import classes from "./MakePrediction.module.scss"
+import "react-datepicker/dist/react-datepicker.css";
 
 const MakePrediction = () => {
+    const [date, setDate] = useState(new Date())
+    const [team1, setTeam1] = useState(null)
+    const [team2, setTeam2] = useState(null)
+
+
+
+    useEffect(() => {
+        // const res = getMatch(date)
+        // console.log(res);
+
+
+    }, [])
+    
   return (
     <div>
-        <TodaysMatch/>
+        <TodaysMatch team1={team1} team2={team2}/>
+        <div className={classes["selectDateBox"]}>
+            <p>Select the date on which you want to see the scheduled matches : </p>
+        <div className={classes["dateBox"]}>
+            <label>Select Date Here : </label>
+            <DatePicker selected={date} onChange={(date) => setDate(date)}/>
+        </div> 
+        </div>
     </div>
   )
 }
