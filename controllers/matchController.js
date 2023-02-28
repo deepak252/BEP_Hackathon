d7ca56c32ffca52c2469734d244add10978cce50
Tnluser <tnluser@Admins-MacBook-Pro.local>
2023-02-28 15:37:53 +0530

    Added time remaining

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -33,12 +33,24 @@ module.exports.getMatchByDate = async (req,res)=>{
         if(!req.body.date){
             return res.status(400).json(errorMessage("Match date is required!"));
         }
-        var date = new Date(req.body.date);
-        var matchDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(),date.getDate(), 11,0,0 )).toISOString()
+        var matchDate = new Date(req.body.date);
+        matchDate.setHours(16,30,0,0);
+
+        // var matchDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(),date.getDate(), 11,0,0 )).toISOString()
         var allMatches = await Match.find({
             matchDate
         }).populate('team1 team2');
-        return res.json(successMessage(allMatches));
+        var currentDate = new Date(new Date().toUTCString())
+        var timeRemaining = (matchDate-currentDate)/1000; // Time remaining in minutes
+        if (timeRemaining < 0)
+            return res.json(successMessage({
+                allMatches
+            }));
+        else 
+        return res.json(successMessage({
+            allMatches,
+            timeRemaining
+        }));
     }catch(err){
         console.log(err)
         return res.status(400).json(errorMessage(err.message));
